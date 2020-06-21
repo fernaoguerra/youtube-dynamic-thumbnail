3b9a96097cf06b850f36abe67812ed090ec44691
Fernao <fernaoguerra@gmail.com>
2020-06-21 05:27:36 +0000

    remove wrong font and edit banner file

--- youtube.py
+++ youtube.py
@@ -2,24 +2,8 @@ import os
 import googleapiclient.discovery
 from apiclient import discovery
 from dotenv import load_dotenv
-# from google.cloud import language_v1
-# from google.cloud.language_v1 import enums
 
 load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "service-account-file.json"
-
-def get_comments():
-    youtube = discovery.build("youtube", "v3", developerKey=os.getenv("GOOGLE_API_KEY"))
-    request = youtube.commentThreads().list(
-        part="Snippet",
-        videoId="_P0VUM28Qsw"
-    )
-    response = request.execute()
-    # print(response)
-    comments = response['items']
-    for comment in comments:
-        c = (comment['snippet']['topLevelComment']['snippet']['textOriginal'])
-        sample_analyze_sentiment(c)
 
 def get_views():
     youtube = discovery.build("youtube", "v3", developerKey=os.getenv("GOOGLE_API_KEY"))
