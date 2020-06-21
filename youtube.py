4e7760678fec6fd4d8c11e12903de7b1ecec2dca
Fernao <fernaoguerra@gmail.com>
2020-06-21 05:58:00 +0000

    removed extra code from youtube.py

--- youtube.py
+++ youtube.py
@@ -16,19 +16,3 @@ def get_views():
     views = response['items'][0]['statistics']['viewCount']
     print (views)
     return views
-    
-    
-def get_youtube_subscribers():
-    youtube = discovery.build("youtube", "v3", developerKey=os.getenv("GOOGLE_API_KEY"))
-    request = youtube.channels().list(
-        part="Snippet,statistics",
-        id="UC0reVIjVRp1i50qiY9lIp4A"
-    )
-    response = request.execute()  
-    subscriber_count = response['items'][0]['statistics']['subscriberCount']
-    print (subscriber_count)
-    return subscriber_count
-    
-
-
-get_views()
